598ace31dc1ccf4306b8eaf9bcd771a4a4e1e923
Mikael Brevik <mikaelbre@gmail.com>
2017-10-19 18:50:27 +0200

    Adds promisify, support for sigint and subjective changes

--- bin/create-ssl-certificate.js
+++ bin/create-ssl-certificate.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+const promisify = require('util').promisify;
 const exec = require('child_process').exec;
 
 let domain = 'dev'
@@ -12,7 +13,7 @@ const colors = {
 }
 
 const commands = {
-  config(domain) {
+  config () {
     return `cat > openssl.cnf <<-EOF
 [req]
 distinguished_name = req_distinguished_name
@@ -29,8 +30,7 @@ DNS.1 = *.\${PWD##*/}.${domain}
 DNS.2 = \${PWD##*/}.${domain}
 EOF`
   },
-  ssl() {
-    return `openssl req \
+  ssl: `openssl req \
 -new \
 -newkey rsa:2048 \
 -sha1 \
@@ -39,36 +39,28 @@ EOF`
 -x509 \
 -keyout ssl.key \
 -out ssl.crt \
--config openssl.cnf`
-  },
-  clean()  {
-    return 'rm openssl.cnf'
-  },
-  keychain() {
-    return 'open /Applications/Utilities/Keychain\\ Access.app ssl.crt'
-  },
-  folder() {
-    return 'open .'
-  }
+-config openssl.cnf`,
+  clean: 'rm openssl.cnf',
+  keychain: 'open /Applications/Utilities/Keychain\\ Access.app ssl.crt',
+  folder: 'open .'
 }
 
-function run (command) {
-  return new Promise((resolve, reject) => {
-    exec(command, (err, stdout, stderr) => {
-      if (err) {
-        reject(err)
-        return
-      }
-      resolve(stdout)
-    })
-  })
+function run (...commands) {
+  return Promise.all(commands.map(cmd => exec(cmd)))
+}
+function runSeries (...commands) {
+  return commands.reduce((p, cmd) =>
+    p.then(() => exec(cmd)), Promise.resolve()
+  )
 }
 
 function pause () {
   return new Promise((resolve, reject) => {
     process.stdin.setRawMode(true);
     process.stdin.resume();
-    process.stdin.on('data', resolve);
+    process.stdin.on('data', buffer => {
+      return buffer[0] === 3 ? reject() : resolve()
+    });
   })
 }
 
@@ -88,29 +80,26 @@ process.argv.forEach(function (val, index, array) {
   }
 });
 
-run(commands.config('dev'))
+runSeries(
+  commands.config(domain),
+  commands.ssl,
+  commands.clean
+)
 .then(() => {
-  return run(commands.ssl())
-})
-.then(() => {
-  return run(commands.clean())
-})
-.then(() => {
-  console.log('\n')
-  console.log(colors.green, 'Certificate created successfully!', colors.cyan, 'Press any key to open Keychain Access and this folder, then:')
-  console.log(colors.white, `
-    1. Drag and drop the created .crt file into Keychain Access -> Certificates
-    2. Double click added certificate -> Trust section
-    3. Change to always trust
-  `)
+  console.log(`
+${colors.green}Certificate created successfully! ${colors.cyan}Press any key to open Keychain Access and this folder, then:
+${colors.white}
+  1. Drag and drop the created .crt file into Keychain Access -> Certificates
+  2. Double click added certificate -> Trust section
+  3. Change to always trust
+`);
   return pause()
 })
-.then(() => {
-  return Promise.all([
-    run(commands.keychain()),
-    run(commands.folder())
-  ])
-})
+.catch(() => process.exit(0))
+.then(() => run(
+  commands.keychain,
+  commands.folder
+))
 .then(() => {
   console.log(colors.cyan, 'Note!', colors.white, 'Make sure you are running "dnsmasq" as described here: https://github.com/christianalfoni/create-ssl-certificate')
   process.exit(0)
