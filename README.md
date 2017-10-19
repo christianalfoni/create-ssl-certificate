cbf045a1cc56da9282865efc5d950e418d427361
Christian Alfoni <christianalfoni@gmail.com>
2017-10-19 10:04:04 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # create-ssl-certificate
 Command line tool to create self signed SSL certificate
+
+**NOTE!** This only works on MAC! Based on the following [amazing GIST](https://gist.github.com/jed/6147872).
+
+## Create
+
+In the folder of your project use the **NPM executer**:
+
+```sh
+npx create-ssl-certificate
+```
+
+This will create a certificate for the domain: **FOLDER_NAME.dev** and any subdomain.
+
+## Options
+
+### Hostname
+
+```sh
+npx create-ssl-certificate --hostname myproject
+```
+
+This will create a certificate for the domain: **myproject.dev** and any subdomain.
+
+### Domain
+
+```sh
+npx create-ssl-certificate --hostname myproject --domain test
+```
+
+This will create a certificate for the domain: **myproject.test** and any subdomain.
+
+## Requirement
+
+You will need to install [dnsmasq](http://www.thekelleys.org.uk/dnsmasq/doc.html) which will route your
+domain to localhost. You can install it via [homebrew](https://brew.sh/index_no.html).
+
+```sh
+brew install dnsmasq
+```
+
+**Note!** Make sure you follow the instructions to start the service when your mac boots up.
+
+To route all `.dev` lookups to localhost you will have to add configuration.
+Replace `dev` in both echo commands if you chose something different
+
+```sh
+mkdir -pv $(brew --prefix)/etc
+sudo mkdir -pv /etc/resolver
+echo "address=/.dev/127.0.0.1" | sudo tee -a $(brew --prefix)/etc/dnsmasq.conf
+echo "nameserver 127.0.0.1" | sudo tee /etc/resolver/dev
+```
---
+++ bin/create-ssl-certificate.js
@@ -0,0 +1,121 @@
+#! /usr/bin/env node
+const exec = require('child_process').exec;
+
+let domain = 'dev'
+let hostname = null
+const colors = {
+  white: '\x1b[37m',
+  green: '\x1b[32m',
+  cyan: '\x1b[36m',
+  red: '\x1b[31m',
+  default: '\x1b[0m'
+}
+
+const commands = {
+  config(domain) {
+    return `cat > openssl.cnf <<-EOF
+[req]
+distinguished_name = req_distinguished_name
+x509_extensions = v3_req
+prompt = no
+[req_distinguished_name]
+CN = *.\${PWD##*/}.${domain}
+[v3_req]
+keyUsage = keyEncipherment, dataEncipherment
+extendedKeyUsage = serverAuth
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = *.\${PWD##*/}.${domain}
+DNS.2 = \${PWD##*/}.${domain}
+EOF`
+  },
+  ssl() {
+    return `openssl req \
+-new \
+-newkey rsa:2048 \
+-sha1 \
+-days 3650 \
+-nodes \
+-x509 \
+-keyout ssl.key \
+-out ssl.crt \
+-config openssl.cnf`
+  },
+  clean()  {
+    return 'rm openssl.cnf'
+  },
+  keychain() {
+    return 'open /Applications/Utilities/Keychain\\ Access.app ssl.crt'
+  },
+  folder() {
+    return 'open .'
+  }
+}
+
+function run (command) {
+  return new Promise((resolve, reject) => {
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        reject(err)
+        return
+      }
+      resolve(stdout)
+    })
+  })
+}
+
+function pause () {
+  return new Promise((resolve, reject) => {
+    process.stdin.setRawMode(true);
+    process.stdin.resume();
+    process.stdin.on('data', resolve);
+  })
+}
+
+function isValid(text, type) {
+  if (typeof text !== 'string' || !text.match(/^[a-zA-Z]+$/)) {
+    console.log(colors.red, `You did not pass in a valid ${type}`)
+    process.exit(1)
+  }
+}
+
+process.argv.forEach(function (val, index, array) {
+  if (val === '--hostname' && isValid(array[index + 1], 'hostname')) {
+    hostname = array[index + 1]
+  }
+  if (val === '--domain' && isValid(array[index + 1], 'domain')) {
+    domain = array[index + 1]
+  }
+});
+
+run(commands.config('dev'))
+.then(() => {
+  return run(commands.ssl())
+})
+.then(() => {
+  return run(commands.clean())
+})
+.then(() => {
+  console.log('\n')
+  console.log(colors.green, 'Certificate created successfully!', colors.cyan, 'Press any key to open Keychain Access and this folder, then:')
+  console.log(colors.white, `
+    1. Drag and drop the created .crf file into Keychain Access -> Certificates
+    2. Double click added certificate -> Trust section
+    3. Change to always trust
+  `)
+  return pause()
+})
+.then(() => {
+  return Promise.all([
+    run(commands.keychain()),
+    run(commands.folder())
+  ])
+})
+.then(() => {
+  console.log(colors.cyan, 'Note!', colors.white, 'Make sure you are running "dnsmasq" as described here: https://github.com/christianalfoni/create-ssl-certificate')
+  process.exit(0)
+})
+.catch((error) => {
+  console.log(colors.red, `Something wrong happened, ${error.message}`)
+  process.exit(1)
+})
